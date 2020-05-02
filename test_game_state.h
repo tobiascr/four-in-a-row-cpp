1baa98cc1087aaa436c146927e2e0dd13369d709
tobiascr <tobiascrw@gmail.com>
2020-05-02 17:08:01 +0200

    modified:   test_game_state.cpp 	modified:   test_game_state.h

--- test_game_state.h
+++ test_game_state.h
@@ -5,26 +5,7 @@
 
 namespace TestEngine
 {
-
-/*  A board configuration is stored as a list of lengths 72. Empty positions are stored as '0'.
-    The players are called '1' and '2', where '1' always make the first move.
-
-    Some of the entries in the list is always '0'. They represent the positions outside of the
-    board, as in the following diagram.
-
-    000000000
-    0xxxxxxx0
-    0xxxxxxx0
-    0xxxxxxx0
-    0xxxxxxx0
-    0xxxxxxx0
-    0xxxxxxx0
-    000000000
-
-    The positions with x are the positions on the board. The position in the lower left
-    corner have index 10, the next index 11 etc.
-
-    Many times in this program, rows and columns are refered to. Rows are counted from
+/*  Many times in this program, rows and columns are refered to. Rows are counted from
     below and numbered 0, 1, ..., 5. Columns are counted from the left and are numbered
     0, 1, ..., 6. Moves are represented by the corresponding columns the moves are made to.*/
 
@@ -60,9 +41,31 @@ public:
     // Return a unique key that corresponds to the current game state.
 
 private:
-    char board[73];
+    /* The places where disks are positioned on the board is stored in bitboards
+    in the form of 64 bit integers. Each player have one bitboard.
+
+    .  .  .  .  .  .  . . .
+    5 12 19 26 33 40 47 . .
+    4 11 18 25 32 39 46 . .
+    3 10 17 24 31 38 45 . .
+    2  9 16 23 30 37 44 . .
+    1  8 15 22 29 36 43 . .
+    0  7 14 21 28 35 42 . . .
+
+    The diagram shows how the bits on the bitboards that corresponds to positions
+    on the board. The first bit is the lowest position to the left. The 48:th bit is
+    the highest position to the right.
+
+    If there is a disk at a position it's bit is 1. If not it's value is 0.
+    All bits not corresponding to positions are 0.
+
+    Using some shifting techniques it's possible to quickly find four in a rows.
+    */
+    uint64_t bitboard_1; // Bitboard for the player that make the first move.
+    uint64_t bitboard_2; // Bitboard for the player that make the second move.
     int column_height[7];
     int number_of_moves;
+    bool player_1_in_turn;
 };
 }
 
