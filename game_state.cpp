3799ba3dda82f8fbf3bd4acf3422b4a66f121fba
tobiascr <tobiascrw@gmail.com>
2021-03-31 23:50:43 +0200

    modified:   game_state.cpp

--- game_state.cpp
+++ game_state.cpp
@@ -83,7 +83,7 @@ void GameState::undo_move_fast(uint64_t move_bitboard)
 
 uint64_t GameState::next_move(int column) const
 {
-    const uint64_t next_moves_ = (bitboard[0] | bitboard[1]) + bottom_row;
+    const uint64_t next_moves = get_next_moves();
     const std::array<uint64_t, 7> columns = {
         0b0000000000000000000000000000000000000000000111111,
         0b0000000000000000000000000000000000001111110000000,
@@ -92,7 +92,7 @@ uint64_t GameState::next_move(int column) const
         0b0000000000000001111110000000000000000000000000000,
         0b0000000011111100000000000000000000000000000000000,
         0b0111111000000000000000000000000000000000000000000};
-    return next_moves_ & columns[column];
+    return next_moves & columns[column];
 }
 
 bool GameState::four_in_a_row() const
@@ -145,7 +145,7 @@ bool GameState::can_win_this_move() const
 {
     const uint64_t b = bitboard[player_in_turn];
 
-    const uint64_t next_moves_ = get_next_moves();
+    const uint64_t next_moves = get_next_moves();
 
     // Masks for various columns.
     const uint64_t mask_0246 = 0b0111111000000001111110000000011111100000000111111;
@@ -156,27 +156,27 @@ bool GameState::can_win_this_move() const
     const uint64_t mask_3 =    0b0000000000000000000000111111000000000000000000000;
 
     // First a test of 4 moves together that might give a false positive.
-    if (four_in_a_row(b | (mask_0246 & next_moves_)))
+    if (four_in_a_row(b | (mask_0246 & next_moves)))
     {
         // To rule out false positives, more tests are done.
 
         // Test column 0 and 4.
-        if (four_in_a_row(b | (mask_04 & next_moves_))) {return true;}
+        if (four_in_a_row(b | (mask_04 & next_moves))) {return true;}
 
         // Test column 2 and 6.
-        if (four_in_a_row(b | (mask_26 & next_moves_))) {return true;}
+        if (four_in_a_row(b | (mask_26 & next_moves))) {return true;}
     }
 
     // First a test of 3 moves together that might give a false positive.
-    if (four_in_a_row(b | (mask_135 & next_moves_)))
+    if (four_in_a_row(b | (mask_135 & next_moves)))
     {
         // To rule out false positives, more tests are done.
 
         // Test column 1 and 5.
-        if (four_in_a_row(b | (mask_15 & next_moves_))) {return true;}
+        if (four_in_a_row(b | (mask_15 & next_moves))) {return true;}
 
         // Test column 3.
-        if (four_in_a_row(b | (mask_3 & next_moves_))) {return true;}
+        if (four_in_a_row(b | (mask_3 & next_moves))) {return true;}
     }
 
     return false;
@@ -264,11 +264,11 @@ uint64_t GameState::get_next_moves() const
 
 uint64_t GameState::get_non_losing_moves() const
 {
-    const uint64_t next_moves_ = get_next_moves();
+    const uint64_t next_moves = get_next_moves();
     const uint64_t opponent_winning_positions =
               get_winning_positions_bitboard(bitboard[1 - player_in_turn])
               & board_mask;
-    const uint64_t blocking_moves = opponent_winning_positions & next_moves_;
+    const uint64_t blocking_moves = opponent_winning_positions & next_moves;
 
     if(blocking_moves)
     {
@@ -288,7 +288,7 @@ uint64_t GameState::get_non_losing_moves() const
             }
         }
     }
-    return next_moves_ & board_mask & (~(opponent_winning_positions >> 1));
+    return next_moves & board_mask & (~(opponent_winning_positions >> 1));
 }
 
 int GameState::open_four_in_a_row_count(int player) const
